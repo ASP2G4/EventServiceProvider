639b40656ddf7618d81831e08e08b3f7de71387a
Winternet <davidcarlstedt@hotmail.com>
2025-05-27 10:28:33 +0200

    added swagger

--- EventServiceProvider/Program.cs
+++ EventServiceProvider/Program.cs
@@ -2,11 +2,13 @@ using EventServiceProvider;
 using EventServiceProvider.Business.Services;
 using EventServiceProvider.Data.Contexts;
 using EventServiceProvider.Data.Repositories;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection")));
@@ -22,8 +24,11 @@ var app = builder.Build();
 
 
 app.MapOpenApi();
+app.UseSwagger();
+app.UseSwaggerUI(x => x.SwaggerEndpoint("/swagger/v1/swagger.json", "Event Service"));
 app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 
+app.UseRewriter(new RewriteOptions().AddRedirect("^$", "swagger"));
 app.UseHttpsRedirection();
 
 
